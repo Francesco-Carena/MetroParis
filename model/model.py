73b8097424e41203f251498b4d876364c8f97a5c
Giuseppe Averta <g.averta3@gmail.com>
2026-04-29 14:23:15 +0200

    lezione del 29/04

--- model/model.py
+++ model/model.py
@@ -1,4 +1,5 @@
 from database.DAO import DAO
+from datetime import datetime
 import networkx as nx
 class Model:
     def __init__(self):
@@ -8,30 +9,74 @@ class Model:
         for f in self._fermate:
             self._idMapFermate[f.id_fermata] = f
 
+    def getBFSNodesFromEdges(self, source):
+        archi = nx.bfs_edges(self._grafo, source)
+        nodiBFS = []
+        for u, v in archi:
+            nodiBFS.append(v)
+        return nodiBFS
+
+    def getBFSNodesFromTree(self, source):
+        tree = nx.bfs_tree(self._grafo, source)
+        archi = list(tree.edges())
+        nodi = list(tree.nodes())
+        return nodi
+
+    def getDFSNodesFromEdges(self, source):
+        archi = nx.dfs_edges(self._grafo, source)
+        nodiDFS = []
+        for u, v in archi:
+            nodiDFS.append(v)
+        return nodiDFS
+
+    def getDFSNodesFromTree(self, source):
+        tree = nx.dfs_tree(self._grafo, source)
+        archi = list(tree.edges())
+        nodi = list(tree.nodes())
+        return nodi
+
     def buildGraph(self):
         self._grafo.clear()
         self._grafo.add_nodes_from(self._fermate)
-        self.addedges3()
 
+        # tic = datetime.now()
+        # self.addedges()
+        # toc = datetime.now()
+        # print("Tempo impiegato da modo 1:", toc-tic)
+        #
+        # tic = datetime.now()
+        # self.addedges2()
+        # toc = datetime.now()
+        # print("Tempo impiegato da modo 2:", toc-tic)
+
+        tic = datetime.now()
+        self.addedges3()
+        toc = datetime.now()
+        print("Tempo impiegato da modo 3:", toc-tic)
 
     def addedges(self):
+        self._grafo.clear_edges()
         for u in self._fermate:
             for v in self._fermate:
                 if DAO.hasconn(u,v):
                     self._grafo.add_edge(u,v)
 
     def addedges2(self):
+        self._grafo.clear_edges()
         for u in self._fermate:
             for conn in DAO.getvicini(u):
                 v = self._idMapFermate[conn.id_stazA]
                 self._grafo.add_edge(u,v)
 
     def addedges3(self):
+        self._grafo.clear_edges()
         alledges=DAO.getAllEdges()
         for conn in alledges:
             u = self._idMapFermate[conn.id_stazP]
             v = self._idMapFermate[conn.id_stazA]
             self._grafo.add_edge(u, v)
+
+
     def get_numnodi(self):
         return len(self._grafo.nodes())
     def get_numarchi(self):
